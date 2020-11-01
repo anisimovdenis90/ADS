a2701441a07ec7dc0e2c2eb1ef2f0c9fc8e65800
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-11-01 21:41:51 +0300

    Подготовка к практической работе №3

---
+++ src/com/anisimov/denis/ads/HomeWork2/myqueue/Queue.java
@@ -0,0 +1,18 @@
+package com.anisimov.denis.ads.HomeWork2.myqueue;
+
+public interface Queue<E> {
+
+    boolean insert(E value);
+
+    E remove();
+
+    E peekHead();
+
+    int size();
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    boolean isFull();
+}
---
+++ src/com/anisimov/denis/ads/HomeWork2/mystack/Stack.java
@@ -0,0 +1,19 @@
+package com.anisimov.denis.ads.HomeWork2.mystack;
+
+public interface Stack<E> {
+
+    void push(E value);
+
+    E pop();
+
+    E peek();
+
+    int size();
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    boolean isFull();
+
+}
