dcae27fc4683b1f38f15ad6ffcd9f5169429b92f
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-11-12 12:59:34 +0300

    Подготовка к практической работе №5. Часть 1

---
+++ src/com/anisimov/denis/ads/HomeWork4/Exponentiation.java
@@ -0,0 +1,23 @@
+package com.anisimov.denis.ads.HomeWork4;
+
+public class Exponentiation {
+
+    public static void main(String[] args) {
+        System.out.println(doExponentiation(2, 3));
+        System.out.println(doExponentiation(2, -3));
+    }
+
+    public static double doExponentiation(int value, int degree) {
+        if (degree > 0) {
+            return exponentiation(value, degree);
+        }
+        return 1 / exponentiation(value, Math.abs(degree));
+    }
+
+    private static double exponentiation(int value, int degree) {
+        if (degree == 0) {
+            return 1;
+        }
+        return value * exponentiation(value, degree - 1);
+    }
+}
