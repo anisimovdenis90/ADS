313f789f1e71bec5df68e4274c38bc08a8ff8831
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-11-13 23:48:22 +0300

    Подготовка к практической работе №6

---
+++ src/com/anisimov/denis/ads/HomeWork5/Tree.java
@@ -0,0 +1,27 @@
+package com.anisimov.denis.ads.HomeWork5;
+
+import java.util.function.Consumer;
+
+public interface Tree<E extends Comparable<? super E>> {
+
+    enum TraverseMode {
+        IN_ORDER,
+        PRE_ORDER,
+        POST_ORDER
+    }
+
+    boolean add(E value);
+
+    boolean contains( E value);
+
+    boolean remove(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void traverse(TraverseMode mode, Consumer<E> action);
+
+    void display();
+
+}
