cf9bb1db984571eea1d61f4ffe6f1daff69d4093
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-11-19 20:07:55 +0300

    Добавлен граф

---
+++ src/com/anisimov/denis/ads/HomeWork6/Vertex.java
@@ -0,0 +1,45 @@
+package com.anisimov.denis.ads.HomeWork6;
+
+import java.util.Objects;
+
+public class  Vertex {
+
+    private final String label;
+    private boolean visited;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    public boolean getVisited() {
+        return visited;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "label='" + label + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(label, vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+}
